0b83ef20dee042cf539169615b39cedf56202c92
vtkiet1011 <votuankiet1011@gmail.com>
2024-03-26 11:31:33 +0700

    Add gpt4free service

--- taskweaver/llm/gpt4free.py
+++ taskweaver/llm/gpt4free.py
@@ -1,3 +1,7 @@
+from g4f.client import Client
+from g4f.Provider import ChatgptAi,You,Liaobots,Bing,ChatForAi,Chatgpt4Online,ChatgptNext,ChatgptX,Gemini,GeminiPro,GptTalkRu,FlowGpt,Koala
+from g4f import get_model_and_provider
+import nest_asyncio
 import os
 from typing import Any, Generator, List, Optional
 
@@ -12,32 +16,42 @@ from .base import CompletionService, EmbeddingService, LLMServiceConfig
 DEFAULT_STOP_TOKEN: List[str] = ["<EOS>"]
 
 
-class OpenAIServiceConfig(LLMServiceConfig):
+# nest_asyncio.apply()
+
+
+class Gpt4freeServiceConfig(LLMServiceConfig):
     def _configure(self) -> None:
         # shared common config
-        self.api_type = self.llm_module_config.api_type
-        assert self.api_type in ["openai", "azure",
-                                 "azure_ad"], "Invalid API type"
+        self._set_name('gpt4free')
 
-        self._set_name(self.api_type)
-
+        self.api_type = self._get_str(
+            "api_type",
+            None,
+        )
         shared_api_base = self.llm_module_config.api_base
         self.api_base = self._get_str(
             "api_base",
-            shared_api_base if shared_api_base is not None else "https://api.openai.com/v1",
+            shared_api_base,
         )
         shared_api_key = self.llm_module_config.api_key
         self.api_key = self._get_str(
             "api_key",
-            shared_api_key if shared_api_key is not None else (
-                "" if self.api_type == "azure_ad" else None),
+            shared_api_key,
         )
 
         shared_model = self.llm_module_config.model
         self.model = self._get_str(
             "model",
-            shared_model if shared_model is not None else "gpt-4",
+            shared_model,
         )
+
+        provider = self._get_str(
+            "provider",
+            None,
+            required=False
+        )
+        _,self.provider = get_model_and_provider(self.model,provider,stream=True)
+
         shared_backup_model = self.llm_module_config.backup_model
         self.backup_model = self._get_str(
             "backup_model",
@@ -58,43 +72,6 @@ class OpenAIServiceConfig(LLMServiceConfig):
             ["openai", "azure", "azure_ad"],
             "openai",
         )
-        is_azure_ad_login = self.api_type == "azure_ad"
-        self.aad_auth_mode = self._get_enum(
-            "aad_auth_mode",
-            ["device_login", "aad_app"],
-            None if is_azure_ad_login else "device_login",
-        )
-
-        is_app_login = is_azure_ad_login and self.aad_auth_mode == "aad_app"
-        self.aad_tenant_id = self._get_str(
-            "aad_tenant_id",
-            None if is_app_login else "common",
-        )
-        self.aad_api_resource = self._get_str(
-            "aad_api_resource",
-            None if is_app_login else "https://cognitiveservices.azure.com/",
-        )
-        self.aad_api_scope = self._get_str(
-            "aad_api_scope",
-            None if is_app_login else ".default",
-        )
-        self.aad_client_id = self._get_str(
-            "aad_client_id",
-            None if is_app_login else "",
-        )
-        self.aad_client_secret = self._get_str(
-            "aad_client_secret",
-            None if is_app_login else "",
-        )
-        self.aad_use_token_cache = self._get_bool("aad_use_token_cache", True)
-        self.aad_token_cache_path = self._get_str(
-            "aad_token_cache_path",
-            "cache/token_cache.bin",
-        )
-        self.aad_token_cache_full_path = os.path.join(
-            self.src.app_base_path,
-            self.aad_token_cache_path,
-        )
 
         self.stop_token = self._get_list("stop_token", DEFAULT_STOP_TOKEN)
         self.temperature = self._get_float("temperature", 0)
@@ -105,27 +82,14 @@ class OpenAIServiceConfig(LLMServiceConfig):
         self.seed = self._get_int("seed", 123456)
 
 
-class OpenAIService(CompletionService, EmbeddingService):
+class Gpt4freeService(CompletionService, EmbeddingService):
     @inject
-    def __init__(self, config: OpenAIServiceConfig):
+    def __init__(self, config: Gpt4freeServiceConfig):
         self.config = config
 
         api_type = self.config.api_type
 
-        assert api_type in ["openai", "azure", "azure_ad"], "Invalid API type"
-
-        self.client: OpenAI = (
-            OpenAI(
-                base_url=self.config.api_base,
-                api_key=self.config.api_key,
-            )
-            if api_type == "openai"
-            else AzureOpenAI(
-                api_version=self.config.api_version,
-                azure_endpoint=self.config.api_base,
-                api_key=(self.config.api_key if api_type == "azure" else self._get_aad_token()),
-            )
-        )
+        self.client: Client = Client(provider=self.config.provider)
 
     def chat_completion(
         self,
@@ -181,7 +145,8 @@ class OpenAIService(CompletionService, EmbeddingService):
                     if delta is None:
                         continue
 
-                    role = delta.role if delta.role is not None else role
+                    # role = delta.role if delta.role is not None else role
+                    role = "assistant"
                     content = delta.content if delta.content is not None else ""
                     if content is None:
                         continue
@@ -237,108 +202,3 @@ class OpenAIService(CompletionService, EmbeddingService):
             model=self.config.embedding_model,
         ).data
         return [r.embedding for r in embedding_results]
-
-    def _get_aad_token(self) -> str:
-        try:
-            import msal  # type: ignore
-        except ImportError:
-            raise Exception(
-                "AAD authentication requires msal module to be installed, please run `pip install msal`",
-            )
-
-        config = self.config
-
-        cache: Any = msal.SerializableTokenCache()
-
-        token_cache_file: Optional[str] = None
-        if config.aad_use_token_cache:
-            token_cache_file = config.aad_token_cache_full_path
-            if not os.path.exists(token_cache_file):
-                os.makedirs(os.path.dirname(token_cache_file), exist_ok=True)
-            if os.path.exists(token_cache_file):
-                with open(token_cache_file, "r") as cache_file:
-                    cache.deserialize(cache_file.read())  # type: ignore
-
-        def save_cache():
-            if token_cache_file is not None and config.aad_use_token_cache:
-                with open(token_cache_file, "w") as cache_file:
-                    cache_file.write(cache.serialize())
-
-        authority = f"https://login.microsoftonline.com/{config.aad_tenant_id}"
-        api_resource = config.aad_api_resource
-        api_scope = config.aad_api_scope
-        auth_mode = config.aad_auth_mode
-
-        if auth_mode == "aad_app":
-            app: Any = msal.ConfidentialClientApplication(
-                client_id=config.aad_client_id,
-                client_credential=config.aad_client_secret,
-                authority=authority,
-                token_cache=cache,
-            )
-            result: Any = app.acquire_token_for_client(
-                scopes=[
-                    f"{api_resource}/{api_scope}",
-                ],
-            )
-            if "access_token" in result:
-                return result["access_token"]
-
-            raise Exception(
-                f"Authentication failed for acquiring AAD token for application login: {str(result)}",
-            )
-
-        scopes = [
-            f"{api_resource}/{api_scope}",
-        ]
-        app: Any = msal.PublicClientApplication(
-            "feb7b661-cac7-44a8-8dc1-163b63c23df2",  # default id in Azure Identity module
-            authority=authority,
-            token_cache=cache,
-        )
-        result = None
-        try:
-            account = app.get_accounts()[0]
-            result = app.acquire_token_silent(scopes, account=account)
-            if result is not None and "access_token" in result:
-                save_cache()
-                return result["access_token"]
-            result = None
-        except Exception:
-            pass
-
-        try:
-            account = cache.find(cache.CredentialType.ACCOUNT)[0]
-            refresh_token = cache.find(
-                cache.CredentialType.REFRESH_TOKEN,
-                query={
-                    "home_account_id": account["home_account_id"],
-                },
-            )[0]
-            result = app.acquire_token_by_refresh_token(
-                refresh_token["secret"],
-                scopes=scopes,
-            )
-            if result is not None and "access_token" in result:
-                save_cache()
-                return result["access_token"]
-            result = None
-        except Exception:
-            pass
-
-        flow = app.initiate_device_flow(scopes=scopes)
-        print(flow["message"])
-        result = app.acquire_token_by_device_flow(flow=flow)
-        if result is not None and "access_token" in result:
-            save_cache()
-            return result["access_token"]
-
-        error_details = "\n".join(
-            [
-                result.get("error"),
-                result.get("error_description"),
-            ],
-        )
-        raise Exception(
-            f"Authentication failed for acquiring AAD token for AAD auth: {error_details}",
-        )
--- taskweaver/llm/test.py
+++ taskweaver/llm/test.py
@@ -1,12 +1,19 @@
 from g4f.client import Client
 from g4f.Provider import ChatgptAi,You,Liaobots,Bing,ChatForAi,Chatgpt4Online,ChatgptNext,ChatgptX,Gemini,GeminiPro,GptTalkRu,Chatgpt4Online,ChatgptNext,FlowGpt
+from g4f import get_model_and_provider
+
+model, provider = get_model_and_provider("gpt-4-turbo", "Bing", stream=True)
 import nest_asyncio
-# nest_asyncio.apply()
+nest_asyncio.apply()
 from g4f.client import Client
 
-client = Client(provider=You)
+client = Client(provider=provider)
 response = client.chat.completions.create(
-    model="gpt-3.5-turbo",
-    messages=[{"role": "user", "content": "Hello"}],
+    model="gpt-4-turbo",
+    messages=[{"role": "user", "content": "Hello, what model are you using ?"}],
+    stream=True
 )
-print(response.choices[0].message.content)
+for res in response:
+    print(res.choices[0].delta.content)
+
+response.close()
